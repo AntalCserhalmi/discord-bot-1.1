2ba4c82beba023be2766357e94b258b6ecadc5d1
Csucsiii <cserhalmiantalprog@gmail.com>
2022-01-24 20:52:08 +0100

    Add try/catch

--- src/modules/events/messageReactionAdd.js
+++ src/modules/events/messageReactionAdd.js
@@ -1,6 +1,8 @@
 const config = require("../../../config.json");
 const { sendEmbedMessage } = require("../functions/embedMessages");
 const { addRole } = require("../functions/roleFunctions");
+const { encrypt } = require("../crypto/crypto");
+
 module.exports = {
     name: "messageReactionAdd",
     once: false,
@@ -15,67 +17,74 @@ module.exports = {
         if (reaction.emoji.name === "✅"){
             client.channels.fetch(config.channel.ids.judge).then(async () => {
                 if (reaction.count >= config.reactions.count){
-                    await addRole(reaction.message.embeds[0].fields[8].value.substring(2,20));
-                    const embedMessage = reaction.message.embeds[0];
-                    embedMessage.setTitle("Elfogadva!");
-                    embedMessage.setColor("#00c911");
-                    embedMessage.fields.push({name: "Legutóbbi javító", value: `<@${user.id}>`});
-    
-                    client.channels.fetch(config.channel.ids.log).then(async channel => {
-                        await channel.send({embeds: [embedMessage]}).catch(console.error);
-                    }).catch(console.error);
+                    try{
+                        await addRole(reaction.message.embeds[0].fields[8].value.substring(2,20));
+                        const embedMessage = reaction.message.embeds[0];
+                        embedMessage.setTitle("Elfogadva!");
+                        embedMessage.setColor("#00c911");
+                        embedMessage.fields.push({name: "Legutóbbi javító", value: `<@${user.id}>`});
+        
+                        client.channels.fetch(config.channel.ids.log).then(async channel => {
+                            await channel.send({embeds: [embedMessage]}).catch(console.error);
+                        }).catch(console.error);
                     
-                    const targetUser = await client.users.fetch(reaction.message.embeds[0].fields[8].value.substring(2,20)).catch(() => null);
+                        const targetUser = await client.users.fetch(reaction.message.embeds[0].fields[8].value.substring(2,20)).catch(() => null);
+                        reaction.message.embeds[0].fields[8].value = `${targetUser.tag} - <@${targetUser.id}>`;
                     
-                    reaction.message.embeds[0].fields[8].value = `${targetUser.tag} - <@${targetUser.id}>`;
-                    
-                    if (targetUser !== null)
-                        targetUser.send({embeds: [
-                            sendEmbedMessage(
-                                "", 
-                                "Sikeres jelentkezés!", 
-                                "Szia! \n\n Jelentkezésed elfogadásra került! \n\nJó időtöltést a szerveren!",
-                                [],
-                                "#00c911", 
-                                false, 
-                                true
-                            )
-                        ]}).catch(console.error);
-    
-                    await reaction.message.delete({timeout: 10000}).catch(console.error);
+                        if (targetUser !== null)
+                            targetUser.send({embeds: [
+                                sendEmbedMessage(
+                                    "", 
+                                    "Sikeres jelentkezés!", 
+                                    "Szia! \n\n Jelentkezésed elfogadásra került! \n\nJó időtöltést a szerveren!",
+                                    [],
+                                    "#00c911", 
+                                    false, 
+                                    true
+                                )
+                            ]}).catch(console.error);
+                            
+                        await reaction.message.delete({timeout: 10000}).catch(console.error);
+                    }catch(err){
+                        await reaction.message.delete({timeout: 10000}).catch(console.error);
+                    }
                 }
             });
         }else if(reaction.emoji.name === "❌"){
             client.channels.fetch(config.channel.ids.judge).then(async () => {
                 if (reaction.count >= config.reactions.count){
-                    const targetUser = await client.users.fetch(reaction.message.embeds[0].fields[8].value.substring(2,20)).catch(() => null);
-                    
-                    reaction.message.embeds[0].fields[8].value = `${targetUser.tag} - <@${targetUser.id}>`;
-                    
-                    client.channels.fetch(config.channel.ids.log).then(async channel => {
-    
-                        const embedMessage = reaction.message.embeds[0];
-                        embedMessage.setTitle("Elutasítva!");
-                        embedMessage.setColor("#c90000");
-                        embedMessage.fields.push({name: "Legutóbbi javító", value: `<@${user.id}>`});
-                        await channel.send({embeds: [embedMessage]}).catch(console.error);
-                    }).catch(console.error)
-    
-    
-                    if (targetUser !== null)
-                        targetUser.send({embeds: [
-                            sendEmbedMessage(
-                                "", 
-                                "Sikertelen jelentkezés!", 
-                                "Szia! \n\n Jelentkezésed sajnos nem sikerült! \n\nKérünk téged, hogy olvass még utána a dolgoknak, és próbálkozz újra 3 óra múlva! \n\nSok sikert!",
-                                [],
-                                "#c90000", 
-                                false, 
-                                true
-                            )
-                        ]}).catch(console.error);
-                    
-                    await reaction.message.delete({timeout: 10000}).catch(console.error);
+                    try{
+                        const targetUser = await client.users.fetch(reaction.message.embeds[0].fields[8].value.substring(2,20)).catch(() => null);
+                        
+                        reaction.message.embeds[0].fields[8].value = `${targetUser.tag} - <@${targetUser.id}>`;
+                        
+                        client.channels.fetch(config.channel.ids.log).then(async channel => {
+        
+                            const embedMessage = reaction.message.embeds[0];
+                            embedMessage.setTitle("Elutasítva!");
+                            embedMessage.setColor("#c90000");
+                            embedMessage.fields.push({name: "Legutóbbi javító", value: `<@${user.id}>`});
+                            await channel.send({embeds: [embedMessage]}).catch(console.error);
+                        }).catch(console.error)
+        
+        
+                        if (targetUser !== null)
+                            targetUser.send({embeds: [
+                                sendEmbedMessage(
+                                    "", 
+                                    "Sikertelen jelentkezés!", 
+                                    "Szia! \n\n Jelentkezésed sajnos nem sikerült! \n\nKérünk téged, hogy olvass még utána a dolgoknak, és próbálkozz újra 3 óra múlva! \n\nSok sikert!",
+                                    [],
+                                    "#c90000", 
+                                    false, 
+                                    true
+                                )
+                            ]}).catch(console.error);
+                        
+                        await reaction.message.delete({timeout: 10000}).catch(console.error);
+                    }catch(err){
+                        await reaction.message.delete({timeout: 10000}).catch(console.error);
+                    }
                 }
             })
         }else if (reaction.emoji.name === "🚀"){
